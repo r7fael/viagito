e99263391b3dcbb6d8db5bed3372b02f2776c698
Rafael Padilha <r7fael@hotmail.com>
2025-06-28 13:54:34 -0300

    update: Atualização LocalRepository filtro por categorias

--- src/main/java/com/project/viagito/viagito/repository/LocalRepository.java
+++ src/main/java/com/project/viagito/viagito/repository/LocalRepository.java
@@ -1,9 +1,14 @@
 package com.project.viagito.viagito.repository;
 
+import com.project.viagito.viagito.model.Category;
 import com.project.viagito.viagito.model.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
 @Repository
 public interface LocalRepository extends JpaRepository<Local, Long> {
+    List<Local> findByCategoryIn(Collection<Category> categories);
 }
