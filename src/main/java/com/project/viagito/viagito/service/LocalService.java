8f8ee47b4eb3844149c10b553f0b7084dd20b65d
Rafael Padilha <r7fael@hotmail.com>
2025-06-28 13:53:46 -0300

    update: Atualização LocalService fórmula de haversine

--- src/main/java/com/project/viagito/viagito/service/LocalService.java
+++ src/main/java/com/project/viagito/viagito/service/LocalService.java
@@ -1,9 +1,12 @@
 package com.project.viagito.viagito.service;
 
+import com.project.viagito.viagito.model.Category;
 import com.project.viagito.viagito.model.Local;
 import com.project.viagito.viagito.repository.LocalRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -56,5 +59,35 @@ public class LocalService {
         else {
             return false;
         }
+    }
+
+
+    private static double haversineService(double lat1, double lon1, double lat2, double lon2) {
+        double earthRadius = 6371;
+
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
+
+        double a = Math.pow(Math.sin(dLat / 2), 2) + Math.pow(Math.sin(dLon / 2), 2) * Math.cos(lat1) * Math.cos(lat2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        return earthRadius * c;
+    }
+
+    public List<Local> findSuggestedLocationsService(Collection<Category> categories, double userLatitude, double userLongitude, double maxKmDistance) {
+        List<Local> locationsByCategory = localRepository.findByCategoryIn(categories);
+
+        List<Local> nearbyLocations = new ArrayList<>();
+
+        for (Local local : locationsByCategory) {
+            double distance = haversineService(userLatitude, userLongitude, local.getLatitude(), local.getLongitude());
+
+            if (distance <= maxKmDistance) {
+                nearbyLocations.add(local);
+            }
+        }
+        return nearbyLocations;
     }
 }
