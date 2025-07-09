2e1a47d4c0f9b5b3bc6d61edaf3446c7215da382
Rafael Padilha <r7fael@hotmail.com>
2025-07-08 21:10:14 -0300

    feat: PreferenceService

---
+++ src/main/java/com/project/viagito/viagito/service/PreferenceService.java
@@ -0,0 +1,41 @@
+package com.project.viagito.viagito.service;
+
+import com.project.viagito.viagito.model.City;
+import com.project.viagito.viagito.model.User;
+import com.project.viagito.viagito.model.UserPreference;
+import com.project.viagito.viagito.repository.CityRepository;
+import com.project.viagito.viagito.repository.UserPreferenceRespository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PreferenceService {
+    private final UserPreferenceRespository userPreferenceRespository;
+    private final CityRepository cityRepository;
+
+    public PreferenceService(UserPreferenceRespository userPreferenceRespository, CityRepository cityRepository) {
+        this.userPreferenceRespository = userPreferenceRespository;
+        this.cityRepository = cityRepository;
+    }
+
+    public Optional<UserPreference> getPreferenceForUserService(User user) {
+        return userPreferenceRespository.findByUser(user);
+    }
+
+    public List<City> getAllCitiesService() {
+        return cityRepository.findAll();
+    }
+
+    public UserPreference saveUpdatePreferences (User user, Long cityId, int distance) {
+        City city = cityRepository.findById(cityId).orElseThrow(() -> new RuntimeException("Cidade não encontrada."));
+
+        UserPreference userPreference = userPreferenceRespository.findByUser(user).orElse(new UserPreference());
+        userPreference.setUser(user);
+        userPreference.setCity(city);
+        userPreference.setMaxDistanceKm(distance);
+
+        return userPreferenceRespository.save(userPreference);
+    }
+}
