0e88016c859eaad95e22f250558c784069003be2
Rafael Padilha <r7fael@hotmail.com>
2025-07-08 21:09:53 -0300

    feat: City model

---
+++ src/main/java/com/project/viagito/viagito/model/City.java
@@ -0,0 +1,19 @@
+package com.project.viagito.viagito.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table (name = "cities")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String state;
+}
