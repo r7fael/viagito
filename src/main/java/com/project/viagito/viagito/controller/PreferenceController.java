94ca89d8efc8ec39b0029da2e51e3f45b71b9cd0
Rafael Padilha <r7fael@hotmail.com>
2025-07-08 21:10:08 -0300

    feat: PreferenceController

---
+++ src/main/java/com/project/viagito/viagito/controller/PreferenceController.java
@@ -0,0 +1,57 @@
+package com.project.viagito.viagito.controller;
+
+import com.project.viagito.viagito.model.City;
+import com.project.viagito.viagito.model.User;
+import com.project.viagito.viagito.model.UserPreference;
+import com.project.viagito.viagito.service.JwtService;
+import com.project.viagito.viagito.service.PreferenceService;
+import com.project.viagito.viagito.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/preferences")
+@RequiredArgsConstructor
+public class PreferenceController {
+    private final PreferenceService preferenceService;
+    private final UserService userService;
+    private final JwtService jwtService;
+
+
+    @GetMapping("/status")
+    public ResponseEntity<?> getUserPreferenceStatusController(@AuthenticationPrincipal User currentUser) {
+        Optional<UserPreference> preferenceOptional = preferenceService.getPreferenceForUserService(currentUser);
+
+        if (preferenceOptional.isPresent()) {
+            UserPreference preferenceEncontrada = preferenceOptional.get();
+            UserPreferenceResponseDTO responseDTO = new UserPreferenceResponseDTO(preferenceEncontrada);
+            return ResponseEntity.ok(responseDTO);
+        } else {
+            return ResponseEntity.noContent().build();
+        }
+    }
+
+    @GetMapping("/cities")
+    public ResponseEntity<List<City>> getAllCitiesController() {
+        List<City> cities = preferenceService.getAllCitiesService();
+        return ResponseEntity.ok(cities);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserPreferenceResponseDTO> saveUserPreferencesController(
+            @RequestBody UserPreferenceRequestDTO requestDTO,
+            @AuthenticationPrincipal User currentUser) {
+
+        UserPreference savedPreference = preferenceService.saveUpdatePreferences(currentUser, requestDTO.getCityId(), requestDTO.getDistance());
+
+        UserPreferenceResponseDTO responseDTO = new UserPreferenceResponseDTO(savedPreference);
+
+        return ResponseEntity.ok(responseDTO);
+    }
+
+}
