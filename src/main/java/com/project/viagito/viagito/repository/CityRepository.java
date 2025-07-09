0a821c61778f1c034eb62b6dc959fcea3c6546bf
Rafael Padilha <r7fael@hotmail.com>
2025-07-08 21:10:00 -0300

    feat: CityRepository

---
+++ src/main/java/com/project/viagito/viagito/repository/CityRepository.java
@@ -0,0 +1,7 @@
+package com.project.viagito.viagito.repository;
+
+import com.project.viagito.viagito.model.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+}
