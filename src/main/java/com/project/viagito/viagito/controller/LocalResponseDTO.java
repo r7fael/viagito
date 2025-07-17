ca0f2e3cd17fde72c86ba32518e00fe8c0b545ee
Rafael Padilha <r7fael@hotmail.com>
2025-07-16 21:02:02 -0300

    update: LocalResponseDTO to get a average rating

--- src/main/java/com/project/viagito/viagito/controller/LocalResponseDTO.java
+++ src/main/java/com/project/viagito/viagito/controller/LocalResponseDTO.java
@@ -9,6 +9,7 @@ public class LocalResponseDTO {
     private String category;
     private double latitude;
     private double longitude;
+    private double averageRating;
 
     public LocalResponseDTO(Local local) {
         this.id = local.getId();
@@ -17,29 +18,32 @@ public class LocalResponseDTO {
         this.category = local.getCategory().toString();
         this.latitude = local.getLatitude();
         this.longitude = local.getLongitude();
+
+        if (local.getAverageRating() != null) {
+            this.averageRating = local.getAverageRating();
+        } else {
+            this.averageRating = 0.0;
+        }
     }
 
     public Long getId() {
         return id;
     }
-
     public String getName() {
         return name;
     }
-
     public String getDescription() {
         return description;
     }
-
     public String getCategory() {
         return category;
     }
-
     public double getLatitude() {
         return latitude;
     }
+    public double getLongitude() { return longitude; }
 
-    public double getLongitude() {
-        return longitude;
+    public double getAverageRating() {
+        return averageRating;
     }
 }
